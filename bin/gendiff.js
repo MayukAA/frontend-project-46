bf4dd6348213b8140600a8fd9b4c4e2f0c4116f4
Andrey Mayuk <mayukda@gmail.com>
2023-02-05 16:31:52 +1000

    Install eslint
    Add .eslintrc.yml
    Update package-lock.json
    Update package.json
    Update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { program } from 'commander';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
