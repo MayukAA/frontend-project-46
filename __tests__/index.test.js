2805379cbda3d6c9ba9afdcdc58f8a0b70a2ad28
Andrey Mayuk <mayukda@gmail.com>
2023-02-19 23:07:06 +1000

    Update __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,16 +9,20 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('diff flat .json', () => {
+const formatPlain = 'plain';
+const expectedFile = readFile('expectedTestFile.txt');
+const expectedFileForPlain = readFile('expectedTestFileForPlain.txt');
+
+test('diff .json', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
-  const expectedFile = readFile('expectedTestFile.txt');
   expect(genDiff(file1, file2)).toEqual(expectedFile.trim());
+  expect(genDiff(file1, file2, formatPlain)).toEqual(expectedFileForPlain.trim());
 });
 
-test('diff flat .yaml', () => {
+test('diff .yaml', () => {
   const file1 = getFixturePath('file1.yaml');
   const file2 = getFixturePath('file2.yaml');
-  const expectedFile = readFile('expectedTestFile.txt');
   expect(genDiff(file1, file2)).toEqual(expectedFile.trim());
+  expect(genDiff(file1, file2, formatPlain)).toEqual(expectedFileForPlain.trim());
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import getParsedFile from './parsers.js';
 import buildTree from './treeBuilder.js';
-import stylish from './formatters/stylish.js';
+// import stylish from './formatters/stylish.js';
+import index from './formatters/index.js';
 
 // const getParsedFile = (filepath) => {
 // return JSON.parse(readFileSync(path.resolve(process.cwd(), filepath), 'utf8'));
 // };
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const file1 = getParsedFile(filepath1);
   const file2 = getParsedFile(filepath2);
 
   const treeDiff = buildTree(file1, file2);
-  return stylish(treeDiff, format);
+  // return stylish(treeDiff, format);
+  return index(treeDiff, formatName);
 };
 
 export default genDiff;
